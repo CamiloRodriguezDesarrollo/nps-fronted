e604917a055f87efcd308ff47f23eb3c5555d4c0
Camilo Programacion <Camilo Programacion@DESKTOP-LIVH0GI>
2026-05-09 17:57:35 -0500

    ajuste en diseño panel principal para votantes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,17 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
+ /* Para agregar el token de autenticacion en las solicitudes http "interceptor" */
+import { authInterceptor } from './interceptors/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient()
+    provideHttpClient(
+      withInterceptors([authInterceptor])
+    )
   ]
 };
